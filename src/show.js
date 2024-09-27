291fcdcebdcf4814f68090e1ee2f9cc46818aa91
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-09-27 17:01:47 +0530

    control show bug fix

--- src/show.js
+++ src/show.js
@@ -1,6 +1,6 @@
-document.getElementsByClassName('ytp-chrome-top')[0].style.visibility = 'hidden';
-document.getElementsByClassName('ytp-chrome-controls')[0].style.visibility = 'hidden';
-document.getElementsByClassName('ytp-gradient-top')[0].style.visibility = 'hidden';
-document.getElementsByClassName('ytp-gradient-bottom')[0].style.visibility = 'hidden';
-// document.getElementsByClassName('ytp-progress-bar')[0].style.visibility = 'hidden';
-document.getElementsByClassName('ytp-progress-bar-container')[0].style.visibility = 'hidden';
+document.getElementsByClassName('ytp-chrome-top')[0].style.visibility = 'visible';
+document.getElementsByClassName('ytp-chrome-controls')[0].style.visibility = 'visible';
+document.getElementsByClassName('ytp-gradient-top')[0].style.visibility = 'visible';
+document.getElementsByClassName('ytp-gradient-bottom')[0].style.visibility = 'visible';
+// document.getElementsByClassName('ytp-progress-bar')[0].style.visibility = 'visible';
+document.getElementsByClassName('ytp-progress-bar-container')[0].style.visibility = 'visible';
