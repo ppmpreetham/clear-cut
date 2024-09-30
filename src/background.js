e5b837387f202ab181926c42203b94e59987e82d
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-09-30 16:13:29 +0530

    minor bug fixes

--- src/background.js
+++ src/background.js
@@ -3,6 +3,16 @@ let controlsHidden = false;
 chrome.commands.onCommand.addListener((command) => {
   if (command === "hide") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+
+      if (tabs.length === 0) {
+        console.error("No active tab found.");
+        return;
+      }
+
       const tab = tabs[0];
       const isYouTube = tab.url.includes("youtube.com/watch?") || tab.url.includes("youtu.be/watch?");
 
@@ -13,9 +23,15 @@ chrome.commands.onCommand.addListener((command) => {
         chrome.scripting.executeScript({
           target: { tabId: tab.id },
           files: [scriptFile]
+        }, () => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          } else {
+            controlsHidden = !controlsHidden;
+          }
         });
-
-        controlsHidden = !controlsHidden;
+      } else {
+        console.log("Not a YouTube video page.");
       }
     });
   }
--- src/show.js
+++ src/show.js
@@ -2,4 +2,3 @@ document.getElementsByClassName('ytp-chrome-top')[0].style.visibility = 'visible
 document.getElementsByClassName('ytp-chrome-controls')[0].style.visibility = 'visible';
 document.getElementsByClassName('ytp-gradient-top')[0].style.visibility = 'visible';
 document.getElementsByClassName('ytp-gradient-bottom')[0].style.visibility = 'visible';
-document.getElementsByClassName('ytp-progress-bar-container')[0].style.visibility = 'visible';
